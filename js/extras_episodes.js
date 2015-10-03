2f476915aac6956289cd75a20704e785e8d03370
tomillie <tomizzi@tomizzi.com>
2015-10-03 02:35:04 +0200

    Little fix of undefined episodes

--- js/extras_episodes.js
+++ js/extras_episodes.js
@@ -26,38 +26,42 @@ function getEpisodes(title) {
         });
     })();
 
-    // create and print the episodes
-    var epContent;
-    var lastPrintedSeason;
-    var seasonTemp;
-    var episodeTemp;
+    if (episodes) {
+        // create and print the episodes
+        var epContent;
+        var lastPrintedSeason;
+        var seasonTemp;
+        var episodeTemp;
 
-    epContent = '<div class="content">' + 
-    				'<table class="epTable">' + 
-						'<tr>' + 
-							'<td>'; 
+        epContent = '<div class="content">' + 
+        				'<table class="epTable">' + 
+    						'<tr>' + 
+    							'<td>'; 
 
-    for (var i = 0; i < episodes.length; i++) {
-	    
-	    if (episodes[i].season % 2 != 0 && episodes[i].season - 1 == lastPrintedSeason) {
-	    	epContent += "</td></tr><tr><td>";
-	    } else if (episodes[i].season - 1 == lastPrintedSeason) {
-	    	epContent += "</td><td>";
-	    }
+        for (var i = 0; i < episodes.length; i++) {
+    	    
+    	    if (episodes[i].season % 2 != 0 && episodes[i].season - 1 == lastPrintedSeason) {
+    	    	epContent += "</td></tr><tr><td>";
+    	    } else if (episodes[i].season - 1 == lastPrintedSeason) {
+    	    	epContent += "</td><td>";
+    	    }
 
-	    seasonTemp = ("0" + episodes[i].season).slice(-2);
-	    episodeTemp = ("0" + episodes[i].number).slice(-2);
+    	    seasonTemp = ("0" + episodes[i].season).slice(-2);
+    	    episodeTemp = ("0" + episodes[i].number).slice(-2);
 
-	    epContent += "<strong>" + seasonTemp + "x" + episodeTemp + "</strong> - " + episodes[i].name + "<br>";
-    	lastPrintedSeason = episodes[i].season;
-    	
-    	if (i == episodes.length - 1) {
-	    	epContent += "</td></tr></table></div>";
-	    }
+    	    epContent += "<strong>" + seasonTemp + "x" + episodeTemp + "</strong> - " + episodes[i].name + "<br>";
+        	lastPrintedSeason = episodes[i].season;
+        	
+        	if (i == episodes.length - 1) {
+    	    	epContent += "</td></tr></table></div>";
+    	    }
 
-    };
+        };
 
-    $("#eph4 > h4").after(epContent);
+        $("#eph4 > h4").after(epContent);
 
-	return true;
+    	return true;
+    } else {
+        return false;
+    }
 }
