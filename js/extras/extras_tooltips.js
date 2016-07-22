4ab82a2a29787f8d34201e7a1aee80b4f906cd2e
tomillie <tomizzi@tomizzi.com>
2016-07-23 00:47:35 +0200

    Better tooltips #39

--- js/extras/extras_tooltips.js
+++ js/extras/extras_tooltips.js
@@ -22,31 +22,30 @@ $(document).ready(function() {
 			}
 
 			function getArtistProfile(url, e) {
-				var isValid = true;
-				$.ajax({
+				return $.ajax({
 					url: url,
 					type: "GET",
-					async: false,
+					async: true,
 					success: function(data) {
-						var content = $(data).find('#profile').html();
-						if (content == null) {
-							isValid = false;
-							return false;
+						// ignore if there are some redirect
+						if (typeof data.redirect == 'undefined') {
+							var content = $(data).find('#profile').html();
+							if (content == null) {
+								return false;
+							}
+							var filmography = "";
+							for (var i = 0; i < 3; i++) {
+								filmography += $(data).find('#filmography .content tr').get(i).outerHTML;
+							}
+							content = content.replace('</ul>', '</ul><br><table>' + filmography + '</table>');
+							$('<div class="artist-tooltip">' + content + '</div>').appendTo('body').fadeIn('fast');
+							$('div.artist-tooltip').css({
+								'top': e.pageY - ($('div.artist-tooltip').height() / 2) - 5,
+								'left': e.pageX + 15
+							});
 						}
-						var filmography = "";
-						for (var i = 0; i < 3; i++) {
-							filmography += $(data).find('#filmography .content tr').get(i).outerHTML;
-						}
-						content = content.replace('</ul>', '</ul><br><table>' + filmography + '</table>');
-						$('<div class="artist-tooltip">' + content + '</div>').appendTo('body').fadeIn('fast');
-						$('div.artist-tooltip').css({
-							'top': e.pageY - ($('div.artist-tooltip').height() / 2) - 5,
-							'left': e.pageX + 15
-						});
 					}
 				});
-
-				return isValid;
 			}
 
 			function createArtistPopup(e) {
@@ -67,11 +66,7 @@ $(document).ready(function() {
 							artist_link_prefix + artist_link + "podle-rating_average/"
 							];
 
-				for (var i = urls.length - 1; i >= 0; i--) {
-					if(getArtistProfile(urls[i], e)) {
-						i = 0;
-					}
-				};
+				tryGetArtistProfile(urls, urls.length - 1, e);
 
 				$(this).bind('mousemove', function(e) {
 					$('div.artist-tooltip').css({
@@ -81,13 +76,31 @@ $(document).ready(function() {
 				});
 			}
 
+			function tryGetArtistProfile(urls, i, e) {
+				getArtistProfile(urls[i], e).done(function(result) {
+					if (typeof result.redirect == 'undefined') {
+						i = 0;
+						return;
+					} else {
+						i--;
+						if (i >= 0) {
+							tryGetArtistProfile(urls, i, e);
+						} else {
+							return;
+						}
+					}
+				});
+			}
+
 			function destroyArtistPopup() {
 				$('div.artist-tooltip').remove();
 			}
 
 			$('[rel=artist-info]').hoverIntent({
 				over: createArtistPopup,
-				out: destroyArtistPopup
+				out: destroyArtistPopup,
+				interval: 300, // delay before 'over'
+				timeout: 500 // delay before 'out'
 			});
 
 			// MOVIE TOOLTIP
@@ -117,58 +130,59 @@ $(document).ready(function() {
 			}
 
 			function getMovieProfile(url, e) {
-				var isValid = true;
-				$.ajax({
+				return $.ajax({
 					url: url,
 					type: "GET",
-					async: false,
+					async: true,
+					cache: true,
 					success: function(data) {
-						var content = "<table border=\"0\"><tr><td>";
-						var poster = $(data).find('.film-poster').get(0);
-						if (typeof poster == 'undefined') {
-							isValid = false;
+						// ignore if there are some redirect
+						if (typeof data.redirect == 'undefined') {
+							var content = "<table border=\"0\"><tr><td>";
+							var poster = $(data).find('.film-poster').get(0);
+							if (typeof poster == 'undefined') {
+								return false;
+							}
+							content += poster.outerHTML;
+							content += "<br>";
+							var rating = $(data).find('#rating .average').text();
+							var ratingNumbersOnly = parseInt(rating);
+							if (ratingNumbersOnly >= 70) {
+								rating = "<div id=\"rating-good\">" + rating +"</div>";
+							} else if (ratingNumbersOnly < 70 && ratingNumbersOnly >= 30) {
+								rating = "<div id=\"rating-average\">" + rating +"</div>";
+							} else if (ratingNumbersOnly < 30) {
+								rating = "<div id=\"rating-trash\">" + rating +"</div>";
+							}
+							content += rating;
+							content += "</td><td>";
+							content += $(data).find('.info h1').get(0).outerHTML;
+							content += $(data).find('.info .genre').get(0).outerHTML;
+							content += $(data).find('.info .origin').get(0).outerHTML;
+							var creators = $(data).find('.info .creators').text();
+							creators = creators.substr(0, 500);
+							creators = creators.replace('Režie:', '<br><strong>Režie:</strong>');
+							creators = creators.replace('Předloha:', '<br><strong>Předloha:</strong>');
+							creators = creators.replace('Kamera:', '<br><strong>Kamera:</strong>');
+							creators = creators.replace('Hudba:', '<br><strong>Hudba:</strong>');
+							creators = creators.replace('Hrají:', '<br><strong>Hrají:</strong>');
+							creators = creators.replace('Scénář:', '<br><strong>Scénář:</strong>');
+							creators += " ...";
+							content += creators;
+							content += "</td></tr></table>"
+							if (content == null) {
+								return false;
+							}
+							$('<div class="movie-tooltip">' + content + '</div>').appendTo('body').fadeIn('fast');
+							$('div.movie-tooltip').css({
+								'top': e.pageY - ($('div.movie-tooltip').height() / 2) - 5,
+								'left': e.pageX + 15
+							});
+						} else {
 							return false;
 						}
-						content += poster.outerHTML;
-						content += "<br>";
-						var rating = $(data).find('#rating .average').text();
-						var ratingNumbersOnly = parseInt(rating);
-						if (ratingNumbersOnly >= 70) {
-							rating = "<div id=\"rating-good\">" + rating +"</div>";
-						} else if (ratingNumbersOnly < 70 && ratingNumbersOnly >= 30) {
-							rating = "<div id=\"rating-average\">" + rating +"</div>";
-						} else if (ratingNumbersOnly < 30) {
-							rating = "<div id=\"rating-trash\">" + rating +"</div>";
-						}
-						content += rating;
-						content += "</td><td>";
-						content += $(data).find('.info h1').get(0).outerHTML;
-						content += $(data).find('.info .genre').get(0).outerHTML;
-						content += $(data).find('.info .origin').get(0).outerHTML;
-						var creators = $(data).find('.info .creators').text();
-						creators = creators.substr(0, 500);
-						creators = creators.replace('Režie:', '<br><strong>Režie:</strong>');
-						creators = creators.replace('Předloha:', '<br><strong>Předloha:</strong>');
-						creators = creators.replace('Kamera:', '<br><strong>Kamera:</strong>');
-						creators = creators.replace('Hudba:', '<br><strong>Hudba:</strong>');
-						creators = creators.replace('Hrají:', '<br><strong>Hrají:</strong>');
-						creators = creators.replace('Scénář:', '<br><strong>Scénář:</strong>');
-						creators += " ...";
-						content += creators;
-						content += "</td></tr></table>"
-						if (content == null) {
-							isValid = false;
-							return false;
-						}
-						$('<div class="movie-tooltip">' + content + '</div>').appendTo('body').fadeIn('fast');
-						$('div.movie-tooltip').css({
-							'top': e.pageY - ($('div.movie-tooltip').height() / 2) - 5,
-							'left': e.pageX + 15
-						});
 					}
 				});
-
-				return isValid;
 			}
 
 			function createMoviePopup(e) {
@@ -202,11 +216,7 @@ $(document).ready(function() {
 							movie_link_prefix + movie_link
 							];
 
-				for (var i = urls.length - 1; i >= 0; i--) {
-					if(getMovieProfile(urls[i], e)) {
-						i = 0;
-					}
-				};
+				tryGetMovieProfile(urls, urls.length - 1, e);
 
 				$(this).bind('mousemove', function(e) {
 					$('div.movie-tooltip').css({
@@ -216,13 +226,31 @@ $(document).ready(function() {
 				});
 			}
 
+			function tryGetMovieProfile(urls, i, e) {
+				getMovieProfile(urls[i], e).done(function(result) {
+					if (typeof result.redirect == 'undefined') {
+						i = 0;
+						return;
+					} else {
+						i--;
+						if (i >= 0) {
+							tryGetMovieProfile(urls, i, e);
+						} else {
+							return;
+						}
+					}
+				});
+			}
+
 			function destroyMoviePopup() {
 				$('div.movie-tooltip').remove();
 			}
 
 			$('[rel=movie-info]').hoverIntent({
 				over: createMoviePopup,
-				out: destroyMoviePopup
+				out: destroyMoviePopup,
+				interval: 300, // delay before 'over'
+				timeout: 500 // delay before 'out'
 			});
 
 		});
