2e00060c176bababdb1c03fd9c7b29a586302bc0
Tomáš Smetanka <tomizzi@tomizzi.com>
2018-10-28 14:46:25 +0100

    #68, #69: Fix links to CSFD movie and creator profiles on IMDB site

--- js/extras/extras_imdb.js
+++ js/extras/extras_imdb.js
@@ -1,58 +1,76 @@
 $(document).ready(function() {
 
-	try
-	{
-		var keys = new Array();
-		keys[0] = "csfdLink";
+    try
+    {
+        var keys = new Array();
+        keys[0] = "csfdLink";
 
-		chrome.storage.local.get(keys, function (result) {
-			var valCsfd = result.csfdLink;
+        chrome.storage.local.get(keys, function (result) {
+            var valCsfd = result.csfdLink;
 
-			// CSFD LINK
-			if (valCsfd == "1" || valCsfd == null) {
+            // CSFD LINK
+            if (valCsfd == "1" || valCsfd == null) {
 
-				var current_url = window.location.href;
-				var profile_type = "";
-				var imdb_id = "";
-				var csfd_url = "";
+                var ProfileType = {
+                    FILM: 1,
+                    CREATOR: 2,
+                    properties: {
+                        1: { urlParam: "film" },
+                        2: { urlParam: "creator" }
+                      }
+                };
 
-				if (current_url.indexOf("/title/") >= 0) {
-					profile_type = "film";
-					imdb_id = current_url.substring(26, 35);
-				} else if (current_url.indexOf("/name/") >= 0) {
-					profile_type = "creator";
-					imdb_id = current_url.substring(25, 34);
-				}
+                var current_url = window.location.href;
+                var profile_type = null;
+                var imdb_id = "";
+                var csfd_url = "";
 
-				if (profile_type != "") {
-					var api_url = "https://api.csfd.cz/imdb/" + profile_type + "/?id=" + imdb_id;
+                if (current_url.indexOf("/title/") >= 0) {
+                    profile_type = ProfileType.FILM;
+                    imdb_id = current_url.substring(27, 36);
+                } else if (current_url.indexOf("/name/") >= 0) {
+                    profile_type = ProfileType.CREATOR;
+                    imdb_id = current_url.substring(26, 35);
+                }
 
-					var json = (function () {
-						$.ajax({
-							'async': false,
-							'global': false,
-							'url': api_url,
-							'dataType': "json",
-							'success': function (data) {
-								if (profile_type == "film") {
-									csfd_url = data.film.web_url;
-								} else {
-									csfd_url = data.creator.web_url;
-								}
-							}
-						});
-					})();
+                if (profile_type != null) {
+                    var api_url = "https://api.csfd.cz/imdb/" + ProfileType.properties[profile_type].urlParam + "/?id=" + imdb_id;
+                    var isFound = false;
 
-					$("#quicklinksMainSection .ghost:last").after('<a href="' + csfd_url + '" class="quicklink imdb_csfd_link">CSFD</a><span class="ghost">|</span>');
-					$(".show_more.quicklink").html('<span class="titleOverviewSprite quicklinksArrowUp"></span>');
-					$(".show_less.quicklink").html('<span class="titleOverviewSprite quicklinksArrowDown"></span>');
-				}
+                    var json = (function () {
+                        $.ajax({
+                            'async': false,
+                            'global': false,
+                            'url': api_url,
+                            'dataType': "json",
+                            'success': function (data) {
+                                if (profile_type == ProfileType.FILM && data.film != null) {
+                                    csfd_url = data.film.web_url;
+                                    isFound = true;
+                                } else if (profile_type == ProfileType.CREATOR && data.creator != null) {
+                                    csfd_url = data.creator.web_url;
+                                    isFound = true;
+                                }
+                            }
+                        });
+                    })();
 
-			}
+                    if (!isFound) {
+                        return;
+                    }
 
-		});
+                    if (profile_type == ProfileType.FILM) {
+                        $("#quicklinksMainSection .ghost:last").after('<a href="' + csfd_url + '" class="quicklink imdb_csfd_link">CSFD</a>');
+                        $(".show_more.quicklink").html('<span class="titleOverviewSprite quicklinksArrowUp"></span>');
+                        $(".show_less.quicklink").html('<span class="titleOverviewSprite quicklinksArrowDown"></span>');
+                    } else {
+                        $(".split_1 .quicklinks .subnav_item_main:last").after('<li class="subnav_item_main"><span class="nobr-only"><a href="' + csfd_url + '" class="link imdb_csfd_link">CSFD</a></span></li>');
+                    }
+                }
+            }
+        });
 
-	} catch (err) {
-		// error
-	}
+    } catch (err) {
+        // error
+    }
 });
