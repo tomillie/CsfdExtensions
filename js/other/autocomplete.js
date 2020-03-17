2b2ff779a5824aa8b3cc450355a7dc8e80924607
tomillie <tomizzi@tomizzi.com>
2020-03-17 05:51:06 +0100

    #72: Improve autocomplete

--- js/other/autocomplete.js
+++ js/other/autocomplete.js
@@ -39,36 +39,38 @@ async function injectAutocomplate() {
                 }
 
                 // if not
-                $.ajax({
-                    url: "https://imdbapi.tomizzi.com/suggest.php",
-                    dataType: "json",
-                    data: {
-                        q: term,
-                        domain: document.domain,
-                        api_key: "2B4DB333ADB11B2A7FEF7C9E1CC33"
-                    },
-                    success: function(data) {
+                let apiParams = {
+                    q: term,
+                    domain: document.domain,
+                    api_key: "2B4DB333ADB11B2A7FEF7C9E1CC33"
+                };
+                let apiUrl = `https://imdbapi.tomizzi.com/suggest.php?${$.param(apiParams)}`;
+
+                fetchAPI(apiUrl)
+                    .then(function(data) {
                         var suggestions = [];
                         var decodedTitle = "";
-                        $.each(data, function(key, val) {
+
+                        $.each(data, function(_key, val) {
                             decodedTitle = decodeHtml(val.title);
                             suggestions.push({ "value": decodedTitle, "csfd_id": val.csfd_id });
                         });
+
                         if (suggestions.length != 0) {
-                            cache[term] = suggestions; // save to cache
+                            cache[term] = suggestions;
                         }
+
                         response(suggestions);
-                    }
-                });
+                    });
             },
             minLength: 2,
             messages: {
                 noResults: "",
                 results: function() { return ""; }
             },
-            open: function(event, ui) { resizePopup(); },
-            close: function(event, ui) { resetPopup(); },
-            select: function(event, ui) {
+            open: function(_event) { resizePopup(); },
+            close: function(_event) { resetPopup(); },
+            select: function(_event, ui) {
                 // if this script is initiated from CSFD.cs search bar
                 if (document.domain == "www.csfd.cz") {
                     window.location.replace("https://www.csfd.cz/film/" + ui.item.csfd_id);
